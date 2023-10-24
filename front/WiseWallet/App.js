4c0b1317e0b3b32c3c26f9e5ecde2cc26af153d6
Linz Pierre <pierre.linz@orange.fr>
2023-10-24 12:35:47 +0200

    initialisation of router

--- front/WiseWallet/App.js
+++ front/WiseWallet/App.js
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreen from "./screens/HomeScreen";
+import AboutScreen from "./screens/AboutScreen";
 
-export default function App() {
+const Stack = createStackNavigator();
+
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="home">
+        <Stack.Screen
+          name="home"
+          component={HomeScreen}
+          options={{ title: "home" }}
+        />
+        <Stack.Screen
+          name="about"
+          component={AboutScreen}
+          options={{ title: "about" }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ front/WiseWallet/screens/AboutScreen.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+function AboutScreen() {
+  return (
+    <View>
+      <Text style={{ fontSize: 20 }}>About Screen</Text>
+    </View>
+  );
+}
+
+export default AboutScreen;
